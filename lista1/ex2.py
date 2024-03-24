a8d7582290f4bb2a776314a25528f5d7e26ae303
gersonpn <gerson.neto@estudante.ifms.edu.br>
2024-03-24 17:04:17 -0400

    ex2

--- lista1/ex2.py
+++ lista1/ex2.py
@@ -1,5 +1,20 @@
-nota = {}
+alunos = []
+
+for _ in range(1):
+    aluno = {}
+    aluno['nome'] = input("Nome: ")
+    aluno['idade'] = input("Idade: ")
+    aluno['notas'] = []
+    for i in range(3):
+        nota = float(input(f"Nota {i+1}: "))
+        aluno['notas'].append(nota)
+    notas = aluno['notas']
+    media = (notas[0] * 1 + notas[1] * 2 + notas[2] * 3) / 6
+    aluno['media'] = round(media, 2)
+    alunos.append(aluno)
+
+for aluno in alunos:
+    aluno.pop('idade')
 
 
-for n in nota:
-  nome
+print(alunos)
