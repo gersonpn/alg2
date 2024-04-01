522ab51681dfb9d9ba7ca9f0f8b7e794fb7de79c
gersonpn <gerson.neto@estudante.ifms.edu.br>
2024-03-31 22:06:43 -0400

    ex11

---
+++ becroowd/ex11.py
@@ -0,0 +1,16 @@
+def vendedor():
+  nome = input()
+  return nome
+
+def pagamento():
+  porcentagem = 15
+  salario = float(input())
+  vendas = float(input())
+  valor = (vendas * porcentagem) / 100
+  salario_total = valor + salario
+  return salario_total
+
+user = vendedor()
+t = pagamento ()
+
+print(f"TOTAL = {t: .2f}")
