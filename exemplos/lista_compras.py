3574fdef7e79cb9caca8f555e1b7c4104adbda84
gersonpn <gerson.neto@estudante.ifms.edu.br>
2024-04-14 22:30:02 -0400

    lista de compras

--- exemplos/lista_compras.py
+++ exemplos/lista_compras.py
@@ -1,23 +1,40 @@
-def compras(lista):
-    opcoes = '\n'.join([f"{i+1}. {item}" for i, item in enumerate(lista)])
-    escolha = input(f"Selecione uma opção:\n{opcoes}\n")
-    return escolha
+def compras():
+    while True:
+        try:
+            opcao = input("[I]nserir [A]pagar [L]istar: ").strip().lower()
+            if opcao not in ["i", "a", "l"]:
+                raise ValueError("Opção inválida. Por favor, escolha entre 'I', 'A' ou 'L'.")
+            return opcao
+        except ValueError as e:
+            print(e)
 
-lista = []
+def inserir(lista):
+        produto = input("Escreva o nome do produto: ")
+        lista.append(produto)
+        return lista
 
-minha_lista = ["inserir", "remover", "listar"]
-opcao_selecionada = compras(minha_lista)
-
-if opcao_selecionada == "1":
-    produto = input("Escreva o nome do produto: ")
-    lista.append(produto)
-
-elif opcao_selecionada == "2":
-    indice = int(input("Qual indice remover? "))
-    if indice < len(lista):
-        lista.pop(indice)
-    else:
+def remover(lista):
+    indice = int(input("Qual item remover? "))
+    try:
+         del lista[indice]
+    except IndexError:
         print("Índice inválido.")
+    return lista
 
-elif opcao_selecionada == "3":
-    print(lista)
+def listar (lista):
+    return lista
+
+
+itens = []
+
+while True:
+    opcao_selecionada = compras()
+    if opcao_selecionada == "i":
+        itens = inserir(itens)
+    elif opcao_selecionada == "a":
+        itens = remover(itens)
+    elif opcao_selecionada == "l":
+        print("Lista de compras:", itens)
+    continuar = input("Deseja continuar? (s/n): ").strip().lower()
+    if continuar != "s" and continuar !=  "sim":
+        break
